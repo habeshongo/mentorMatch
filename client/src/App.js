174bf6db9cfb0dcfab68d420bd9c4f74f6c6ac82
Medhanie Alene <alenemedhanie@gmail.com>
2023-08-02 18:41:45 -0400

    clean app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,23 +7,9 @@ import Profile from './Components/profile';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <LoginButton />
+      <LoginButton />
         <LogoutButton />
         <Profile />
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
