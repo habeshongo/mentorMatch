c574a916a3fd4514491d7bb2d7c11c38161ccb02
Medhanie Alene <alenemedhanie@gmail.com>
2023-08-10 21:24:34 -0400

    add connections, requests & messages components and routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,11 @@ import Home from './Components/home';
 import LoginButton from './Components/login';
 import LogoutButton from './Components/logout';
 import Profile from './Components/profile';
+import { Connections } from './Components/connections';
 import EditProfileForm from "./Components/EditProfileForm";
 import { BrowserRouter } from 'react-router-dom'
+import { Requests } from './Components/requests';
+import { Messages } from './Components/messages';
 
 
 function App() {
@@ -17,6 +20,9 @@ function App() {
           <Route path="/logout" element={<LogoutButton />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/edit" element={<EditProfileForm />} />
+          <Route path="/connections" element={<Connections />} />
+          <Route path="/requests" element={<Requests />} />
+          <Route path="/messages" element={<Messages />} />
           </Routes>
         </BrowserRouter>
     </div>
---
+++ client/src/Components/connections.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Connections = () => {
+  return (
+    <div>connections</div>
+  )
+}
